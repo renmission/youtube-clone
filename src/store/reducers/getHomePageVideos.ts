51fea1deaf4131291a09ed63a737b0b5644ec54f
Ren Mission <missionrenjr@gmail.com>
2023-03-23 21:08:03 +0800

    updating

--- src/store/reducers/getHomePageVideos.ts
+++ src/store/reducers/getHomePageVideos.ts
@@ -1,21 +1,26 @@
-import { YOUTUBE_API_URL } from "./../../utils/constants";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "..";
 import axios from "axios";
+import { YOUTUBE_API_URL } from "./../../utils/constants";
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+// const API_KEY = process.env.REACT_APP_YOUTUBE_DATA_API_KEY;
+const API_KEY = 'AIzaSyCzpDBMcZl4tH_nla6hAJ_D586J-pyrttc';
 
 export const getHomePageVideos = createAsyncThunk(
-  "youtubeApp/homePageVideos",
+  "youtubeApp/homePageVidoes",
   async (isNext: boolean, { getState }) => {
     const {
-      youtubeApp: { nextPageToken: nextPageTokenFormState, videos },
+      youtubeApp: { nextPageToken: nextPageTokenFromState, videos },
     } = getState() as RootState;
     const {
       data: { items, nextPageToken },
     } = await axios.get(
-      `${YOUTUBE_API_URL}/search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=snippet&type=video`
+      `${YOUTUBE_API_URL}search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=snippet&type=video&${
+        isNext ? `pageToken=${nextPageTokenFromState}` : ""
+      }`
     );
-    console.log(items);
+    console.log({ items, nextPageTokenFromState, nextPageToken });
+    // const parsedData: HomePageVideos[] = await parseData(items);
+    // return { parsedData: [...videos, ...parsedData], nextPageToken };
   }
 );
